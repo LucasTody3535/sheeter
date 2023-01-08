6a30f3342cc5b78c09bca767ff15f1d24a84e9e0
LucasTody3535 <lucas.web3535@gmail.com>
2023-01-08 11:08:10 -0300

    add: utils to get table column headings and their values

--- src/utils/table-coordinates/table-coordinates.ts
+++ src/utils/table-coordinates/table-coordinates.ts
@@ -13,4 +13,25 @@ export const isInitialCoordinateBelowThanFinalCoordinate = (initialCoord: string
  return Number(initialCoord.trim().substring(1)) < Number(finalCoord.trim().substring(1));
 }
 
+export const getTableHeadings = (data: Array<HTMLInputElement>): Array<string> => {
+ const headings = new Array<string>();
+ const headingsSet = new Set<string>();
+ data.forEach(el => {
+  if(el.value.trim().length > 0) {
+    headingsSet.add(`${el.dataset.cell!.substring(0, 1)}1`);
+  }
+ });
+ headingsSet.forEach(el => headings.push(el));
+ return headings;
+}
+
+export const getTableHeadingValues = (data: Array<string>, tableCells: Array<HTMLInputElement>): Array<string> => {
+ const headingsValues: Array<string> = [];
+ data.forEach((el) => {
+  if(tableCells.find((tableCell) => tableCell.dataset.cell === el)!.value) {
+   headingsValues.push(tableCells.find((tableCell) => tableCell.dataset.cell === el)!.value);
+  } else headingsValues.push("unknown");
+ })
+ return headingsValues;
+}
 
